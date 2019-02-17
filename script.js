19e161a306d9ea0522b0eee5db43bcbbedb26d33
Ivan M <cs77677321@gmail.com>
2019-02-17 22:34:05 +0800

    Add connection sound effect

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ connectTime = 1500;
 var fakeBar = document.getElementById('fake-bar')
 var statusText = document.getElementById('status-text')
 
-
 function ProgressBarAnimation(){
     
     if (fakeBar.style.width == "100%"){
@@ -29,6 +28,9 @@ function ProgressBarAnimation(){
 //>>>>> Fake Connection >>>>>
 
 async function CheckConnection(username, password){
+    
+    var sound = document.getElementById('sound-effect')
+    var texts;
 
     await ProgressBarAnimation();
     
@@ -39,36 +41,65 @@ async function CheckConnection(username, password){
     if (username == "admin" && password == ""){
         fakeBar.classList.add("bg-success")
         statusText.innerHTML = "Access granted.";
-        //Remove connection panel
-        $("#connection-panel").slideUp(2000)
+        sound.src = "audio/Success.mp3"
+        sound.play();
+        $("#connection-panel").slideUp(2000) //Remove connection panel
+        /*
         document.getElementById('fade-text-1').innerHTML = "Congratulations! You have just gained access to our website.";
         document.getElementById('fade-text-2').innerHTML = "You identifed yourself as an legitimate user, therefore you can access the content of this website";
         document.getElementById('fade-text-3').innerHTML = "This is called authentication"
+        */
+        texts = [
+            "AAA",
+            "BBB",
+            "CCC"
+        ]
 
     // Access denied
     } else {
         fakeBar.classList.add("bg-danger")
-        statusText.innerHTML = "Access denied!";   
+        statusText.innerHTML = "Access denied!"; 
+        sound.src = "audio/Fail.mp3"
+        sound.play();
+        /*
         document.getElementById('fade-text-1').innerHTML = "You seem to have entered invalid information.";
         document.getElementById('fade-text-2').innerHTML = "Now try to login as an administrator";
         document.getElementById('fade-text-3').innerHTML = "Type both username and password as 'admin' and submit again";
+        */
+        texts = [
+            "111",
+            "222",
+            "333"
+        ]
         
     }
     
     //Start fading text
-    TextFade();
+    TextFade(texts);
 }
 
 //<<<<< Fake Connection <<<<<
 //>>>>> Text Fade >>>>>
 
-function TextFade(){
+function TextFade(texts){
+    /*
     $("#fade-bg").fadeIn("slow", function(){
         $("#fade-text-1").delay(300).fadeIn("slow").delay(3000).fadeOut("slow", function(){
             $("#fade-text-2").fadeIn("slow").delay(3000).fadeOut("slow", function(){
                 $("#fade-text-3").fadeIn("slow")
             })
         })
+    })
+    */
+    var text = document.getElementsByClassName("fade-text");
+    
+    $("#fade-bg").fadeIn("slow", function(){
+        $.each(texts, function(index, value){
+            text.innerHTML = value;
+            $(".fade-text").fadeIn("slow", function(){
+                var nxt = texts.next
+            })
+        
     })
 
 }          
